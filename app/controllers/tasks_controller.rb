08c231746833a7baeb79a8819c6014b0a4282f53
cedric.J <cedric_pro@mailoo.org>
2021-11-10 00:07:42 +0100

    rails-task-manager_notComplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,48 @@
 class TasksController < ApplicationController
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    def create
+      @task = Restaurant.new(task_params)
+      if @task.save
+        redirect_to task_path(@task)
+      else
+        render :new
+      end
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    task.update(task_params)
+    redirect_to task_path(@task) # @task important ? faut pas ?
+  end
+
+  def destroy
+    task.destroy
+    redirect_to task_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  # Permet de ne pas repeter @task = Task.find(params[:id]) dans show edit update destroy + code apres class en haut
+  def set_tasks
+    set_tasks = Task.find(params[:id])
+  end
 end
